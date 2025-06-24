1b25c09141c604b5edeaa9e393c4da0f93363515
dabbh <hagben@gmail.com>
2025-06-24 14:34:32 +0300

    הוספתי addplayers

--- sol.c
+++ sol.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAXPL 15
 #define NL 3
 typedef enum {premier,national,artzit} league;
@@ -32,4 +33,15 @@ Sports *initseason(){
 	s->numallpl = 0;
 	s->allPls = NULL;
 	return s;
+}
+Player *addPlayer(Sports *s,const char *name,int height,int year){
+	Player *p = malloc(sizeof(Player));
+	p->name = malloc(strlen(name) + 1);
+	strcpy(p->name, name);
+	p->height = height;
+	p->YOB = year;
+	p->ID = s->numallpl++;
+	s->allPls = realloc(s->allPls, s->numallpl * sizeof(Player*));
+	s->allPls[s->numallpl - 1] = p;
+	return p;
 }
