0c74054be08023f49f97d2756da611e4df8e659f
dabbh <hagben@gmail.com>
2025-06-24 14:26:39 +0300

    הוספתי את הטטראקטים

--- sol.c
+++ sol.c
@@ -1,1 +1,24 @@
-#define MAX_SIZE 1
+#define MAXPL 15
+#define NL 3
+typedef enum {premier,national,artzit} league;
+typedef struct {
+	char *name;
+	int ID;
+	int height;
+	int YOB;
+} Player;
+typedef struct Team {
+	char *name;
+	Player *players[MAXPL];
+	int shirts[MAXPL];
+	int numpl;
+} Team;
+typedef struct Lteam {
+	Team *myT;
+	struct Lteam *next;
+} Lteam;
+typedef struct Sports {
+	Lteam *league[NL];
+	int numallpl;
+	Player **allPls;
+} Sports;
